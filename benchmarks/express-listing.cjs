7eec23667993cfeb5b36e828136183f197ac61c9
jain_chetan_spinny <chetan.jain@spinny.com>
2024-02-06 19:43:33 +0530

    added middlewares

--- benchmarks/express-listing.cjs
+++ benchmarks/express-listing.cjs
@@ -2,6 +2,12 @@
 const json = require('./big-json.json')
 
 const express = require('express')
+const helmet = require('helmet')
+const hpp = require('hpp')
+const compression = require('compression')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const path = require('path')
 
 const app = express()
 
@@ -13,5 +19,23 @@ app.use(require('cors')())
 app.get('/', function (req, res) {
   res.json(json)
 })
+app.use(cookieParser())
+// Use helmet to secure Express with various HTTP headers
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    crossOriginEmbedderPolicy: false,
+    crossOriginResourcePolicy: false,
+    crossOriginOpenerPolicy: false
+  })
+)
+// Prevent HTTP parameter pollution
+app.use(hpp())
+// Compress all requests
+app.use(compression())
+// global config
+// Use for http request debug (show errors only)
+app.use(logger('dev', { skip: (_, res) => res.statusCode < 400 }))
+app.use(express.static(path.resolve(process.cwd(), 'public')))
 
 app.listen(8000)
--- benchmarks/fastify-listing.cjs
+++ benchmarks/fastify-listing.cjs
@@ -2,6 +2,26 @@
 
 const fastify = require('fastify')()
 const json = require('./big-json.json')
+const fastifyApp = require('fastify')()
+const path = require('path')
+
+fastifyApp.register(require('@fastify/helmet'), {
+  contentSecurityPolicy: false,
+  crossOriginEmbedderPolicy: false,
+  crossOriginResourcePolicy: false,
+  crossOriginOpenerPolicy: false,
+  global: true
+})
+
+// Compress all requests
+fastifyApp.register(require('@fastify/compress'), { global: false })
+
+// Use for http request debug (show errors only)
+fastifyApp.register(require('@fastify/static'), {
+  root: path.join(process.cwd(), 'public'),
+  wildcard: false,
+  prefix: '/' // optional: default '/'
+})
 fastify.get('/', function (request, reply) {
   reply.send(json)
 })
