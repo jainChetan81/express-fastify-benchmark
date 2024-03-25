48c815bd34195da363842456ccd971e2888449cf
jainChetan81 <jain.cj.chetan@gmail.com>
2024-03-25 16:08:42 +0530

    updated tests

--- benchmarks/fastify-listing-json.cjs
+++ benchmarks/fastify-listing-json.cjs
@@ -1,276 +1,31 @@
-'use strict'
-const opts = {
-  schema: {
-    response: {
-      200: {
-        type: 'object',
-        properties: {
-          listingDetail: {
-            type: 'object',
-            properties: {
-              marketing_data: {
-                type: 'string'
-              },
-              breadcrumbs: {
-                type: 'array',
-                items: {
-                  type: 'object',
-                  properties: {
-                    link: {
-                      type: 'string'
-                    },
-                    text: {
-                      type: 'string'
-                    }
-                  },
-                  required: ['link', 'text']
-                }
-              },
-              no_car_found_data: {
-                type: 'string'
-              },
-              meta_data: {
-                type: 'object',
-                properties: {
-                  title: {
-                    type: 'string'
-                  },
-                  description: {
-                    type: 'string'
-                  },
-                  keywords: {
-                    type: 'string'
-                  },
-                  og_title: {
-                    type: 'string'
-                  },
-                  og_description: {
-                    type: 'string'
-                  },
-                  canonical_url: {
-                    type: 'string'
-                  },
-                  filter_url: {
-                    type: 'string'
-                  },
-                  robots: {
-                    type: 'string'
-                  },
-                  og_image: {
-                    type: 'string'
-                  }
-                },
-                required: ['title', 'description', 'keywords', 'og_title', 'og_description', 'canonical_url', 'filter_url', 'robots', 'og_image']
-              },
-              applied_filters_with_display_name: {
-                type: 'object',
-                properties: {
-                  category: {
-                    type: 'array',
-                    items: {
-                      type: 'object',
-                      properties: {
-                        key: {
-                          type: 'string'
-                        },
-                        display_name: {
-                          type: 'string'
-                        }
-                      },
-                      required: ['key', 'display_name']
-                    }
-                  },
-                  product_type: {
-                    type: 'array',
-                    items: {
-                      type: 'object',
-                      properties: {
-                        key: {
-                          type: 'string'
-                        },
-                        display_name: {
-                          type: 'string'
-                        }
-                      },
-                      required: ['key', 'display_name']
-                    }
-                  },
-                  city: {
-                    type: 'array',
-                    items: {
-                      type: 'object',
-                      properties: {
-                        key: {
-                          type: 'string'
-                        },
-                        display_name: {
-                          type: 'string'
-                        }
-                      },
-                      required: ['key', 'display_name']
-                    }
-                  }
-                },
-                required: ['category', 'product_type', 'city']
-              },
-              schema_data: {
-                type: 'object',
-                properties: {
-                  breadcrumb_data: {
-                    type: 'object',
-                    properties: {
-                      context: {
-                        type: 'string'
-                      },
-                      type: {
-                        type: 'string'
-                      },
-                      itemListElement: {
-                        type: 'array',
-                        items: {
-                          type: 'object',
-                          properties: {
-                            type: {
-                              type: 'string'
-                            },
-                            item: {
-                              type: 'object',
-                              properties: {
-                                id: {
-                                  type: 'string'
-                                },
-                                name: {
-                                  type: 'string'
-                                }
-                              },
-                              required: ['id', 'name']
-                            },
-                            position: {
-                              type: 'integer'
-                            }
-                          },
-                          required: ['type', 'item', 'position']
-                        }
-                      }
-                    },
-                    required: ['context', 'type', 'itemListElement']
-                  }
-                },
-                required: ['breadcrumb_data']
-              },
-              footer_table_heading: {
-                type: 'string'
-              },
-              metaObject: {
-                type: 'object',
-                properties: {
-                  topHeading: {
-                    type: 'string'
-                  },
-                  topContent: {
-                    type: 'string'
-                  },
-                  faqListing: {
-                    type: 'array',
-                    items: {
-                      type: 'object',
-                      properties: {
-                        question: {
-                          type: 'string'
-                        },
-                        answer: {
-                          type: 'string'
-                        }
-                      },
-                      required: ['question', 'answer']
-                    }
-                  }
-                },
-                required: ['topHeading', 'topContent', 'faqListing']
-              },
-              noCarData: {
-                type: 'object',
-                properties: {
-                  noCarsFoundArray: {
-                    type: 'array',
-                    items: {
-                      type: 'object',
-                      properties: {
-                        labelKey: {
-                          type: 'string'
-                        },
-                        labelValue: {
-                          type: 'string'
-                        }
-                      },
-                      required: ['labelKey', 'labelValue']
-                    }
-                  },
-                  noCarsFoundText: {
-                    type: 'string'
-                  }
-                },
-                required: ['noCarsFoundArray', 'noCarsFoundText']
-              },
-              applied_filters: {
-                type: 'object',
-                properties: {
-                  city: {
-                    type: 'array',
-                    items: {
-                      type: 'string'
-                    }
-                  },
-                  product_type: {
-                    type: 'array',
-                    items: {
-                      type: 'string'
-                    }
-                  },
-                  category: {
-                    type: 'array',
-                    items: {
-                      type: 'string'
-                    }
-                  }
-                },
-                required: ['city', 'product_type', 'category']
-              }
-            }
-          },
-          constantData: {
-            type: 'object'
-          }
-        }
-      }
-    }
-  }
-}
+"use strict";
 
-const fastify = require('fastify')()
-const json = require('./big-json.json')
-const fastifyApp = require('fastify')()
-const path = require('path')
+const { opts } = require("./schema.cjs");
 
-fastifyApp.register(require('@fastify/helmet'), {
+const fastify = require("fastify")();
+const json = require("./big-json.json");
+const fastifyApp = require("fastify")();
+const path = require("path");
+
+fastifyApp.register(require("@fastify/helmet"), {
   contentSecurityPolicy: false,
   crossOriginEmbedderPolicy: false,
   crossOriginResourcePolicy: false,
   crossOriginOpenerPolicy: false,
-  global: true
-})
+  global: true,
+});
 
 // Compress all requests
-fastifyApp.register(require('@fastify/compress'), { global: false })
+fastifyApp.register(require("@fastify/compress"), { global: false });
 
 // Use for http request debug (show errors only)
-fastifyApp.register(require('@fastify/static'), {
-  root: path.join(process.cwd(), 'public'),
+fastifyApp.register(require("@fastify/static"), {
+  root: path.join(process.cwd(), "public"),
   wildcard: false,
-  prefix: '/' // optional: default '/'
-})
-fastify.get('/', opts, function (request, reply) {
-  reply.send(json)
-})
+  prefix: "/", // optional: default '/'
+});
+fastify.get("/", opts, function (request, reply) {
+  reply.send(json);
+});
 
-fastify.listen({ port: 3000, host: '127.0.0.1' })
+fastify.listen({ port: 3000, host: "127.0.0.1" });
