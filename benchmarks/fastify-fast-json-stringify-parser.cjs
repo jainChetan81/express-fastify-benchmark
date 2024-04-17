f1ff81e5d2071fc64d6ea673243d064bc5c4339a
jainChetan81 <jain.cj.chetan@gmail.com>
2024-04-17 20:13:25 +0530

    updated naming

--- benchmarks/fastify-fast-json-stringify-parser.cjs
+++ benchmarks/fastify-fast-json-stringify-parser.cjs
@@ -8,11 +8,11 @@ const fastifyApp = require("fastify")();
 const path = require("path");
 
 fastifyApp.register(require("@fastify/helmet"), {
-  contentSecurityPolicy: false,
-  crossOriginEmbedderPolicy: false,
-  crossOriginResourcePolicy: false,
-  crossOriginOpenerPolicy: false,
-  global: true,
+	contentSecurityPolicy: false,
+	crossOriginEmbedderPolicy: false,
+	crossOriginResourcePolicy: false,
+	crossOriginOpenerPolicy: false,
+	global: true
 });
 
 // Compress all requests
@@ -20,12 +20,12 @@ fastifyApp.register(require("@fastify/compress"), { global: false });
 
 // Use for http request debug (show errors only)
 fastifyApp.register(require("@fastify/static"), {
-  root: path.join(process.cwd(), "public"),
-  wildcard: false,
-  prefix: "/", // optional: default '/'
+	root: path.join(process.cwd(), "public"),
+	wildcard: false,
+	prefix: "/" // optional: default '/'
 });
 fastify.get("/", opts, function (request, reply) {
-  reply.send(json);
+	reply.send(json);
 });
 
 fastify.listen({ port: 3000, host: "127.0.0.1" });
